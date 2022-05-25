8efad0c4a71e0ef26151c9b88c6a044f12e1784c
hrisha ko <hrisha.ko@gmail.com>
2022-05-25 19:55:21 +0300

    app/script.py:20:155: E0001: unexpected character after line continuation character

--- app/script.py
+++ app/script.py
@@ -17,7 +17,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         for item, value in os.environ.items():
             self.wfile.write(bytes("<div style='color:red;font-size:15px;'> \
-            %s:</div> <div style='color:black;font-size:15px;'> %s </div>" \ 
+            %s:</div> <div style='color:black;font-size:15px;'> %s </div>" 
             % (item, value), "utf-8"))
         self.wfile.write(bytes("<p>----------------------</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
