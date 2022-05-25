1405da53882fe195c288b79f012dfa4e08444057
hrisha ko <hrisha.ko@gmail.com>
2022-05-25 20:55:04 +0300

    Lint warnings correction

--- app/script.py
+++ app/script.py
@@ -1,3 +1,6 @@
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
 """System module."""
 import os
 
@@ -13,18 +16,16 @@ class MyServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>VARIABLES:</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>----------------------</p>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         for item, value in os.environ.items():
-            self.wfile.write(bytes("<div style='color:red;font-size:15px;'> \
-            %s:</div> <div style='color:black;font-size:15px;'> %s </div>" 
-            % (item, value), "utf-8"))
+            self.wfile.write(bytes(f"<div style='color:red;font-size:15px;'> \
+            {item}:</div> <div style='color:black;font-size:15px;'>{value}</div>", "utf-8"))
         self.wfile.write(bytes("<p>----------------------</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
     webServer = HTTPServer((HOST_NAME, SERVER_PORT), MyServer)
-    print("Server started http://%s:%s" % (HOST_NAME, SERVER_PORT))
+    print("Server started http://{HOST_NAME}:{SERVER_PORT}")
 
     try:
         webServer.serve_forever()
