3fb340cd3051cf81cb5a494c9000f1d7672237ec
hrisha ko <hrisha.ko@gmail.com>
2022-06-03 23:53:18 +0300

    MODIFY snyk_installing.sh script.

--- scripts/snyk_installing.sh
+++ scripts/snyk_installing.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-latest_version=$(curl -Is "https://github.com/snyk/snyk/releases/latest" | grep "^location" | sed s#.*tag/##g | tr -d "\r")
+latest_version="$(curl -sIL https://github.com/snyk/snyk/releases/latest | grep "^location:" |grep "tag" | sed 's/[^v0-9.]*//g' | cut -c 2-)"
 echo "Latest Snyk CLI Version: ${latest_version}"
 snyk_cli_dl_linux="https://github.com/snyk/snyk/releases/download/${latest_version}/snyk-linux"
 echo "Download URL: ${snyk_cli_dl_linux}"
